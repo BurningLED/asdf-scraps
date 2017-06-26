0d86bdc44b981797861c6e7da2495949c353a651
LED <bskrobisz@gmail.com>
2017-06-26 19:24:58 -0400

    hullo

---
+++ RFtest/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFtest_on_git
+{
+    public class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, world!");
+    
+         // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+}
